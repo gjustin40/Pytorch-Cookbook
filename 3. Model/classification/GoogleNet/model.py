52e7f27842f7b133f1b10c59185690624bdf5aae
gjustin <gjustin@naver.com>
2021-10-08 08:47:06 +0900

    Update model.py

--- 3. Model/classification/GoogleNet/model.py
+++ 3. Model/classification/GoogleNet/model.py
@@ -3,51 +3,105 @@ import torch.nn as nn
 
 
 class GoogLeNet(nn.Module):
-    def __init__(self, in_channels=3, num_classes=1000):
+    def __init__(self, in_channels=3, num_classes=1000, conv_block=None):
         super(GoogLeNet, self).__init__()
         
         self.in_channels = in_channels
-        
-        self.conv1 = conv_block(self.in_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.num_classes = num_classes
+        if conv_block is None:
+            self.conv_block = BasicConv2d
+            
+        self.conv1 = self.conv_block(self.in_channels, 64, kernel_size=7, stride=2, padding=3)
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv2 = conv_block(64, 192, kernel_size=3, padding=1)
+        self.conv2 = self.conv_block(64, 192, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
-        self.inception3a = inception_block(64, 96, 128, 16, 32, 32)
+        self.inception3a = inception_block(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = inception_block(256, 128, 128, 192, 32, 96, 64)
         
+        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
+        self.inception4a = inception_block(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = inception_block(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = inception_block(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = inception_block(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = inception_block(528, 256, 160, 320, 32, 128, 128)
+        
+        self.pool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.inception5a = inception_block(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = inception_block(832, 384, 192, 384, 48, 128, 128)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout2d(0.4)
+        self.fc = nn.Linear(1024, self.num_classes)
+        
+    def forward(self, x):
+        
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
 
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+
+        x = self.pool3(x)
+
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        x = self.inception4e(x)
+
+        x = self.pool4(x)
+
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+
+        x = self.avgpool(x)
+        
+        x = x.reshape(x.shape[0], -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        
+        return x
+
+    
 class inception_block(nn.Module):
-    def __init__(self, in_channels, out1_1x1, out3_1x1, out_3x3, out5_1x1, out_5x5, outpool_1x1):
+    def __init__(self, in_channels, out1_1x1, out3_1x1, out_3x3, out5_1x1, out_5x5, outpool_1x1, conv_block=None):
         super(inception_block, self).__init__()
         
         self.in_channels = in_channels
-        
-        self.branch1 = conv_block(in_channels, out1_1x1, kernel_size=1)
+        if conv_block is None:
+            self.conv_block = BasicConv2d
+            
+        self.branch1 = self.conv_block(self.in_channels, out1_1x1, kernel_size=1)
         
         self.branch2 = nn.Sequential(
-            conv_block(self.in_channels, out3_1x1, kernel_size=1),
-            conv_block(out3_1x1, out_3x3, kernel_size=3, padding=1)
+            self.conv_block(self.in_channels, out3_1x1, kernel_size=1),
+            self.conv_block(out3_1x1, out_3x3, kernel_size=3, padding=1)
         )
         
         self.branch3 = nn.Sequential(
-            conv_block(self.in_channels, out5_1x1, kernel_size=1),
-            conv_block(out5_1x1, out_5x5, kernel_size=5, padding=2)
+            self.conv_block(self.in_channels, out5_1x1, kernel_size=1),
+            self.conv_block(out5_1x1, out_5x5, kernel_size=5, padding=2)
         )
         
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.conv_block(self.in_channels, outpool_1x1, kernel_size=1)
+            self.conv_block(self.in_channels, outpool_1x1, kernel_size=1)
         )
         
     def forward(self, x):
-        out = torch.cat([self.branch(x), self.branch2(x), self.branch3(x), self.branch4(x)], 1)
+        out = torch.cat([self.branch1(x), self.branch2(x), self.branch3(x), self.branch4(x)], 1)
         
         return out
     
-class conv_block(nn.Module):
+    
+class BasicConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
-        super(conv_block, self).__init__()
+        super(BasicConv2d, self).__init__()
         
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -62,8 +116,8 @@ class conv_block(nn.Module):
         return out
     
 
-x = torch.randn(1, 3, 224, 224)
-conv1 = conv_block(3, 64, kernel_size=7, stride=2, padding=3)
-# maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-print(conv1(x).shape)
-# print(maxpool(x).shape)
+if __name__ == '__main__':
+    x = torch.randn(8, 3, 843, 134)
+    model = GoogLeNet()
+    print(model(x).shape)
+      
