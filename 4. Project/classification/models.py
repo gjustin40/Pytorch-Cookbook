9c5a0e174fa6c06271128e68c30088640672f094
gjustin <gjustin@naver.com>
2021-11-02 01:18:19 +0900

    Update models.py

--- 4. Project/classification/models.py
+++ 4. Project/classification/models.py
@@ -3,23 +3,33 @@ import torchvision.models as m
 
 def get_model(name, num_classes, pretrained=False):
     
-    if name == 'vgg11':  
-        model = m.vgg11_bn(pretrained=pretrained)
+    # if name == 'vgg11':  
+    #     model = m.vgg11_bn(pretrained=pretrained)
     
-    elif name == 'vgg13': 
-        model = m.vgg13_bn(pretrained=pretrained)
+    # elif name == 'vgg13': 
+    #     model = m.vgg13_bn(pretrained=pretrained)
     
-    elif name == 'vgg16':        
-        model = m.vgg16_bn(pretrained=pretrained)
+    # elif name == 'vgg16':        
+    #     model = m.vgg16_bn(pretrained=pretrained)
 
-    elif name == 'vgg19': 
-        model = m.vgg19_bn(pretrained=pretrained)
+    # elif name == 'vgg19': 
+    #     model = m.vgg19_bn(pretrained=pretrained)
         
-    for p in model.parameters():
-        p.requires_grad = False
-    model.classifier[-1] = nn.Linear(4096, num_classes)
+    # for p in model.parameters():
+    #     p.requires_grad = False
+    # model.classifier[-1] = nn.Linear(4096, num_classes)
+    
+    
+    model = m.densenet121(pretrained=pretrained)
+    num_ftrs = model.classifier.in_features
+    model.classifier = nn.Sequential(
+        nn.Linear(num_ftrs, 500),
+        nn.Linear(500, 2)
+    )
     
     return model
+
+
 
 if __name__ == '__main__':
     model = get_model('vgg11', 2, pretrained=True)
