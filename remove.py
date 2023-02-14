52a7f4e8c053c775d8c6c90ab0926540b2e1f7cf
gjustin <gjustin@naver.com>
2023-02-14 15:22:13 +0900

    commit

---
+++ remove.py
@@ -0,0 +1,9 @@
+from time import time, sleep, strftime, gmtime
+import datetime
+from time import strftime
+
+import torch
+torch.set_printoptions(precision=5, sci_mode=False)
+
+a = torch.tensor([0.0000e+00, 1.1396e-05, 0.0000e+00, 0.0000e+00])
+print(a)
